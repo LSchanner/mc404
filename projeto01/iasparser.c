1f642044ba8f4929886a93def2e711de0d821237
Lucas Schanner <lsschanner@gmail.com>
2015-09-17 01:01:20 -0300

    coisa pracarai

--- projeto01/iasparser.c
+++ projeto01/iasparser.c
@@ -8,6 +8,8 @@ typedef struct Palavra{
         long long dado;
         long instrucoes[2];
     };
+    bool used = 0;
+    bool is_data = 0;
 }Palavra;
 
 typedef struct MemoryMapImpl{
@@ -19,13 +21,20 @@ typedef struct posicao_montagem{
     int pos_instrucao;
 }PosicaoMontagem;
 
-bool match_rotulo(String,bool declaration);
-bool match_instrucao(String);
+// Funções utilitárias
+bool match_rotulo(String);
+
+bool match_symbol(String);
+
 int parse_num(String);
 
-Tabela Instrucoes;
+void write_word(MemoryMapImpl map,PosicaoMontagem pos,long long data);
 
-MemoryMap parse_ias_string(String input, Tabela rotulos,bool first){
+void write_instruction(MemoryMapImpl map,PosicaoMontagem pos,long data);
+
+void erro(String message);
+
+MemoryMap parse_ias_string(String input, Tabela rotulos,Tabela symbols,bool first){
     char* token;
     PosicaoMontagem pos = {0,0};
     MemoryMapImpl mapa;
@@ -34,15 +43,82 @@ MemoryMap parse_ias_string(String input, Tabela rotulos,bool first){
     token = strtok(input," \t");
     while(token != NULL){
 
-        if(match_rotulo(token,true)){
+        if(match_rotulo(token)){
             Registro rotulo = {token,(void*) pos};
             InsereTabela(rotulos, rotulo);
             token = strtok(NULL," \t");
         }
 
+        // Diretivas
         if(token[0] == '.'){
-        
+            // Diretiva .org
+            if(strcmp(token,".org") == 0){
+                token = strtok(NULL," \t");
+                pos.pos_mapa = parse_num(token);
+                pos.pos_instrucao = 0;
 
+            // Diretiva .set
+            }else if(strcmp(token,".set") == 0){
+                String symbol = strtok(NULL," \t");
+                if(match_symbol(symbol)){
+                    long long value = parse_num(strtok(NULL," \t"));
+                    Registro macro = {symbol,(void*)value};
+                    InsereTabela(symbols,macro);
+                }else{
+                    erro("Simbolo inválido",pos);
+                }
+                
+
+            // Diretiva .align
+            }else if(strcmp(token,".align") == 0){
+                int num = parse_num(strtok(NULL," \t"));
+                while(pos.pos_mapa % num != 0 && pos.pos_instrucao != 0){
+
+                    write_instruction(mapa,pos,0);
+                    if(pos.pos_instrucao == 1){
+                        pos.pos_instrucao = 0;
+                        pos.pos_mapa++;
+                    }else{
+                        pos.pos_instrucao = 1;
+                    }
+                }
+
+            // Diretiva .wfill
+            }else if(strcmp(token,".wfill") == 0){
+                int num = parse_num(strtok(NULL," \t,"));
+                long long data;
+                Registro reg;
+                token = strtok(NULL," \t");
+                if(first){
+                    data = 0;
+                }else if(ConsultaTabela(symbols,token,&reg)){
+                    data = (long long) reg.val;
+                }else if(ConsultaTabela(rotulos,token,&reg)){
+                    data = (long long) reg.val;
+                }
+
+                for(int i = 0; i < data; i++){
+                    write_word(mapa,pos,num);
+                    pos.pos_mapa++;
+                }
+
+            // Diretiva .word
+            }else if(strcmp(token,".word") == 0){
+                long long data;
+                Registro reg;
+                token = strtok(NULL," \t");
+                if(first){
+                    data = 0;
+                }else if(ConsultaTabela(symbols,token,&reg)){
+                    data = (long long) reg.val;
+                }else if(ConsultaTabela(rotulos,token,&reg)){
+                    data = (long long) reg.val;
+                }
+                write_word(mapa,pos,data);
+                pos.pos_mapa++;
+            }
+
+        }else if(strcmp(token,"ADD")){
         }
     }
 
