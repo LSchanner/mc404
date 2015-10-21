157e211fe468e3c3923672d41d55284769fd619e
Lucas Schanner <lsschanner@gmail.com>
2015-10-21 15:23:34 -0200

    lab07

---
+++ lab07/impl.s
@@ -0,0 +1,46 @@
+	@ Global symbol
+        .global set_speed_motor
+        .global set_speed_motors
+        .global read_sonar
+        .global read_sonars
+
+	.align 4
+set_speed_motor:
+    stmfd sp!, {r7, lr}	@ Save the callee-save registers
+		                        @ and the return address.
+    cmp r1, #0
+    beq set_motor0
+set_motor1:
+    mov r7, #127
+    svc 0x0
+    b end
+
+set_motor0:
+    mov r7, #126
+    svc 0x0
+end:
+    ldmfd sp!, {r7, pc} @ Restore the registers and return
+
+set_speed_motors:
+    stmfd sp!, {r7, lr}  @ Save the callee-save registers
+		              @ and the return address.
+    mov r7, #124
+    svc 0x0
+    ldmfd sp!, {r7, pc}  @ Restore the registers and return
+
+read_sonar:
+    stmfd sp!, {r7, lr}  @ Save the callee-save registers
+    mv r7, #125
+    svc 0x0
+    ldmfd sp!, {r7, pc}  @ Restore the registers and return
+
+read_sonars:
+    stmfd sp!, {r4-r11, lr}  @ Save the callee-save registers
+    mov r2, r0
+    mov r0, #0
+loop_sonars:
+    mv r7, #125
+    svc 0x0
+
+
+    ldmfd sp!, {r4-r11, pc}  @ Restore the registers and return
